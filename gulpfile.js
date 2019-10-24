c2db69cef6d353d8d415701b82edd329854991ff
alex <alex_kobilianskiy@bigmir.net>
2019-10-24 22:18:40 +0300

    section blog complete

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('rsync', function() {
 	.pipe(rsync({
 		root: 'app/',
 		hostname: 'alexko89@alexko89.beget.tech',
-		destination: 'demos.alexko89.beget.tech/public_html/dirname/',
+		destination: 'demos.alexko89.beget.tech/public_html/tatto/',
 		// include: ['*.htaccess'], // Includes files to deploy
 		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
 		recursive: true,
