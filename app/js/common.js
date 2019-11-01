ecdb9c69527f61a0e22afd955accccced08db077
alex <alex_kobilianskiy@bigmir.net>
2019-11-01 22:21:18 +0200

    page-product.html complete

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
 
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
     /**
      * mobile-mnu customization
      */
@@ -72,37 +104,47 @@ $(document).ready(function(){
 
     });
 
-    $('img.svg').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
 
-        jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
 
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-
-            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-        }, 'xml');
+    $('.product-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        items: 1,
+        margin: 30,
+        dots: false,
+        autoHeight: false,
+        navText: ['', '']
     });
+
+    // $('.product-slider').on('translate.owl.carousel', function(event) {
+    //     var currentImg = $('.owl-item.active').find('.product-slide');
+    //     var color = currentImg.data('fill');
+    //
+    //     $('#smear .svg').css('fill', color);
+    // console.log(color);
+    // });
+
+
+
+
+
+
+
+
+    if($('#smear').length){
+        console.log('begin');
+        var color = $('#smear').data('fill');
+        console.log(color);
+
+        var tt = $('#smear ').find('svg');
+        console.log(tt);
+        setTimeout(function () {
+            $('#smear .svg').css('fill', color);
+        }, 1000)
+
+    }
+
+
 
 
 
